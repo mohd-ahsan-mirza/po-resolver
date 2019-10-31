6a206da6adf454bba92f1ad9c660f4eb9aff12ee
mohd-ahsan-mirza <mohd.ahsan.mirza@gmail.com>
2019-10-31 22:49:52 +0000

    Generating resulting po file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
-def parse_po_file(file_name):
-    file_object =  open(file_name,"r")
+def parse_po_file():
     pos = []
     for run in range(1,len(sys.argv)):
+        file_object =  open(sys.argv[run],"r")
         index = 0
         po = []
         po.append("")
@@ -13,7 +13,23 @@ def parse_po_file(file_name):
             else:
                 po[index] = po[index] + line
         pos.append(po)
+    return pos
+def generate_combined_po(pos):
+    result = []
+    for po in pos:
+        for element in po:
+            if element not in result:
+                result.append(element)
+    return result
+def generate_result_file(result):
+    with open('result.po', 'w') as f:
+        for item in result:
+            f.write("%s\n" % item)
+
 if __name__ == '__main__':
     #print(sys.argv)
-    parse_po_file(sys.argv[1])
+    pos = parse_po_file()
+    result = generate_combined_po(pos)
+    generate_result_file(result)
+
 
