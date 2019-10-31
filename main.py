502d247ffc98999ddff186dd167bacd8706ae2a6
mohd-ahsan-mirza <mohd.ahsan.mirza@gmail.com>
2019-10-31 21:58:58 +0000

    Adding functionality to script

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import sys
+def parse_po_file(file_name):
+    file_object =  open(file_name,"r")
+    result = []
+    for line in file_object:
+        print(line)
 if __name__ == '__main__':
-    print("Coming in main")
+    #print(sys.argv)
+    parse_po_file(sys.argv[1])
+
