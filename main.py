35da9a11ef15510021d14261ba4a921863f123fa
mohd-ahsan-mirza <mohd.ahsan.mirza@gmail.com>
2019-10-31 22:26:04 +0000

    Parsing through pos

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import sys
 def parse_po_file(file_name):
     file_object =  open(file_name,"r")
-    result = []
-    for line in file_object:
-        print(line)
+    pos = []
+    for run in range(1,len(sys.argv)):
+        index = 0
+        po = []
+        po.append("")
+        for line in file_object:
+            if not line.strip():
+                index = index + 1
+                po.append("")
+            else:
+                po[index] = po[index] + line
+        pos.append(po)
 if __name__ == '__main__':
     #print(sys.argv)
     parse_po_file(sys.argv[1])
